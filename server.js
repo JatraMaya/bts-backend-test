1073c9a2a1c85b23ec3f2803d5821c7e22cd5294
JatraMaya <ahmad.f.iksan@gmail.com>
2021-05-18 15:57:32 +0800

    add server.js and adding necessary package

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const app = express("app")
+const readdirp = require('readdirp')
 
 app.use(express.json())
+
+readdirp(".", {
+    fileFilter: "*Route.js"
+}).on("data", (route) => {
+    const path = `./{route.path}`
+    app.use(require(path))
+    console.log(`${path} is loaded`)
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`\n==== Server run on ${PORT} ====\n`)
+})
